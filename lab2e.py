71c1168b93b4641153dad28ffb451d17047e3c60
joy otchere <joy17blue@gmail.com>
2024-09-26 00:57:04 -0400

    completed lab

---
+++ lab2e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author: Joy Otchere
+# Author ID: JoyOtchere
+# Date Created: 2024/09/26
+
+# Set timer value
+timer = 10
+
+# Loop until timer reaches 0
+while timer > 0:
+    print(timer)  # Print the current timer value
+    timer -= 1    # Decrease the timer
+
+print("blast off!")  # Print blast off when timer reaches 0
+
+
