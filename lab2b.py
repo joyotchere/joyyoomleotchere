2c888b5397ce6afa93c8aa18dc1eb0f91c0b2857
joy otchere <joy17blue@gmail.com>
2024-09-25 23:12:49 -0400

    completed lab

---
+++ lab2b.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Prompt the user for their name and age
+name = input("Name: ")
+age = input("Age: ")
+
+#Print the message
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+
+
+
